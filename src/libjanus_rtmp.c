39209f3d2f45d067bdb53dffaff8099ada525a02
Aleksey Gureev <spyromus@noizeramp.com>
2020-12-09 16:46:56 +0300

    Proper scoping of components

--- src/libjanus_rtmp.c
+++ src/libjanus_rtmp.c
@@ -25,55 +25,53 @@
 #include <errno.h>
 #include <gst/gst.h>
 
-#include "pipeline.h"
-
 /* Plugin information */
-#define PLUGIN_VERSION         1
-#define PLUGIN_VERSION_STRING  "0.0.1"
-#define PLUGIN_DESCRIPTION     "This is a live streaming plugin for Janus, allowing WebRTC peers to send their media to RTMP servers via Gstreamer."
-#define PLUGIN_NAME            "JANUS SH live video plugin"
-#define PLUGIN_AUTHOR          "agureiev@shakuro.com"
-#define PLUGIN_PACKAGE         "janus.plugin.rtmp"
+#define PLUGIN_RTMP_VERSION         1
+#define PLUGIN_RTMP_VERSION_STRING  "0.0.1"
+#define PLUGIN_RTMP_DESCRIPTION     "This is an RTMP streaming plugin for Janus, allowing WebRTC peers to send their media to RTMP servers via Gstreamer."
+#define PLUGIN_RTMP_NAME            "Janus RTMP plugin"
+#define PLUGIN_RTMP_AUTHOR          "agureiev@shakuro.com"
+#define PLUGIN_RTMP_PACKAGE         "janus.plugin.rtmp"
 
 /* Plugin methods */
 janus_plugin *create(void);
-int plugin_init(janus_callbacks *callback, const char *config_path);
-void plugin_destroy(void);
-int plugin_get_api_compatibility(void);
-int plugin_get_version(void);
-const char *plugin_get_version_string(void);
-const char *plugin_get_description(void);
-const char *plugin_get_name(void);
-const char *plugin_get_author(void);
-const char *plugin_get_package(void);
-void plugin_create_session(janus_plugin_session *handle, int *error);
-struct janus_plugin_result *plugin_handle_message(janus_plugin_session *handle, char *transaction, json_t *message, json_t *jsep);
-void plugin_setup_media(janus_plugin_session *handle);
-void plugin_hangup_media(janus_plugin_session *handle);
-void plugin_destroy_session(janus_plugin_session *handle, int *error);
-json_t *plugin_query_session(janus_plugin_session *handle);
+int plugin_rtmp_init(janus_callbacks *callback, const char *config_path);
+void plugin_rtmp_destroy(void);
+int plugin_rtmp_get_api_compatibility(void);
+int plugin_rtmp_get_version(void);
+const char *plugin_rtmp_get_version_string(void);
+const char *plugin_rtmp_get_description(void);
+const char *plugin_rtmp_get_name(void);
+const char *plugin_rtmp_get_author(void);
+const char *plugin_rtmp_get_package(void);
+void plugin_rtmp_create_session(janus_plugin_session *handle, int *error);
+struct janus_plugin_result *plugin_rtmp_handle_message(janus_plugin_session *handle, char *transaction, json_t *message, json_t *jsep);
+void plugin_rtmp_setup_media(janus_plugin_session *handle);
+void plugin_rtmp_hangup_media(janus_plugin_session *handle);
+void plugin_rtmp_destroy_session(janus_plugin_session *handle, int *error);
+json_t *plugin_rtmp_query_session(janus_plugin_session *handle);
 
 
 
-static janus_plugin plugin_plugin =
+static janus_plugin janus_rtmp_plugin =
   JANUS_PLUGIN_INIT (
-    .init = plugin_init,
-    .destroy = plugin_destroy,
+    .init = plugin_rtmp_init,
+    .destroy = plugin_rtmp_destroy,
 
-    .get_api_compatibility = plugin_get_api_compatibility,
-    .get_version = plugin_get_version,
-    .get_version_string = plugin_get_version_string,
-    .get_description = plugin_get_description,
-    .get_name = plugin_get_name,
-    .get_author = plugin_get_author,
-    .get_package = plugin_get_package,
+    .get_api_compatibility = plugin_rtmp_get_api_compatibility,
+    .get_version = plugin_rtmp_get_version,
+    .get_version_string = plugin_rtmp_get_version_string,
+    .get_description = plugin_rtmp_get_description,
+    .get_name = plugin_rtmp_get_name,
+    .get_author = plugin_rtmp_get_author,
+    .get_package = plugin_rtmp_get_package,
 
-    .create_session = plugin_create_session,
-    .handle_message = plugin_handle_message,
-    .setup_media = plugin_setup_media,
-    .hangup_media = plugin_hangup_media,
-    .destroy_session = plugin_destroy_session,
-    .query_session = plugin_query_session,
+    .create_session = plugin_rtmp_create_session,
+    .handle_message = plugin_rtmp_handle_message,
+    .setup_media = plugin_rtmp_setup_media,
+    .hangup_media = plugin_rtmp_hangup_media,
+    .destroy_session = plugin_rtmp_destroy_session,
+    .query_session = plugin_rtmp_query_session,
   );
 
 static volatile gint initialized = 0, stopping = 0;
@@ -81,21 +79,21 @@ static volatile gint next_port = 11000;
 
 /* Plugin creator */
 janus_plugin *create(void) {
-  JANUS_LOG(LOG_VERB, "%s created!\n", PLUGIN_NAME);
-  return &plugin_plugin;
+  JANUS_LOG(LOG_VERB, "%s created!\n", PLUGIN_RTMP_NAME);
+  return &janus_rtmp_plugin;
 }
 
 /* Parameter validation */
-static struct janus_json_parameter request_parameters[] = {
+static struct janus_json_parameter plugin_rtmp_request_parameters[] = {
   {"request", JSON_STRING, JANUS_JSON_PARAM_REQUIRED}
 };
-static struct janus_json_parameter start_live[] = {
+static struct janus_json_parameter plugin_rtmp_start_live[] = {
   {"url", JSON_STRING, JANUS_JSON_PARAM_REQUIRED}
 };
 
 
 /** Sessions */
-typedef struct plugin_session {
+typedef struct plugin_rtmp_session {
   janus_plugin_session *handle;
 
   gboolean started;
@@ -103,7 +101,7 @@ typedef struct plugin_session {
   GstBus *bus;
 
   janus_mutex mutex;
-} plugin_session;
+} plugin_rtmp_session;
 static GHashTable *sessions;
 static janus_mutex sessions_mutex;
 
@@ -114,25 +112,26 @@ static janus_mutex sessions_mutex;
 #define ERROR_MISSING_ELEMENT   413
 #define ERROR_UNKNOWN_ERROR     499
 
+
 /* Internal functions forward declaration. */
-void stop_session_pipeline(plugin_session *session);
+static void stop_session_pipeline(plugin_rtmp_session *session);
 static gboolean bus_callback(GstBus * bus, GstMessage * message, gpointer data);
-json_t *create_error_response(int error_code, char *error_cause);
-plugin_session *session_from_handle(janus_plugin_session *handle);
-gboolean is_valid_url(const char *url);
+static plugin_rtmp_session *session_from_handle(janus_plugin_session *handle);
+static gboolean is_valid_url(const char *url);
+static GstElement *start_pipeline(const char* url, int audio_port, int video_port);
 
 /* Message handlers. */
-janus_plugin_result *handle_message(plugin_session *session, json_t *root);
-janus_plugin_result *handle_message_start(plugin_session *session, json_t* root);
-janus_plugin_result *handle_message_stop(plugin_session *session, json_t *root);
+static janus_plugin_result *handle_message(plugin_rtmp_session *session, json_t *root);
+static janus_plugin_result *handle_message_start(plugin_rtmp_session *session, json_t* root);
+static janus_plugin_result *handle_message_stop(plugin_rtmp_session *session, json_t *root);
 
 
 // ------------------------------------------------------------------------------------------------
 // Plugin implementation
 // ------------------------------------------------------------------------------------------------
 
-int plugin_init(janus_callbacks *callback, const char *config_path) {
-  JANUS_LOG(LOG_INFO, "%s initialized!\n", PLUGIN_NAME);
+int plugin_rtmp_init(janus_callbacks *callback, const char *config_path) {
+  JANUS_LOG(LOG_INFO, "%s initialized!\n", PLUGIN_RTMP_NAME);
   sessions = g_hash_table_new(NULL, NULL);
   g_atomic_int_set(&initialized, 1);
 
@@ -141,56 +140,56 @@ int plugin_init(janus_callbacks *callback, const char *config_path) {
   return 0;
 }
 
-void plugin_destroy(void) {
-  JANUS_LOG(LOG_INFO, "%s destroyed!\n", PLUGIN_NAME);
+void plugin_rtmp_destroy(void) {
+  JANUS_LOG(LOG_INFO, "%s destroyed!\n", PLUGIN_RTMP_NAME);
   g_hash_table_destroy(sessions);
 
   gst_deinit();
 }
 
-int plugin_get_api_compatibility(void) {
+int plugin_rtmp_get_api_compatibility(void) {
   return JANUS_PLUGIN_API_VERSION;
 }
 
-int plugin_get_version(void) {
-  return PLUGIN_VERSION;
+int plugin_rtmp_get_version(void) {
+  return PLUGIN_RTMP_VERSION;
 }
 
-const char *plugin_get_version_string(void) {
-  return PLUGIN_VERSION_STRING;
+const char *plugin_rtmp_get_version_string(void) {
+  return PLUGIN_RTMP_VERSION_STRING;
 }
 
-const char *plugin_get_description(void) {
-  return PLUGIN_DESCRIPTION;
+const char *plugin_rtmp_get_description(void) {
+  return PLUGIN_RTMP_DESCRIPTION;
 }
 
-const char *plugin_get_name(void) {
-  return PLUGIN_NAME;
+const char *plugin_rtmp_get_name(void) {
+  return PLUGIN_RTMP_NAME;
 }
 
-const char *plugin_get_author(void) {
-  return PLUGIN_AUTHOR;
+const char *plugin_rtmp_get_author(void) {
+  return PLUGIN_RTMP_AUTHOR;
 }
 
-const char *plugin_get_package(void) {
-  return PLUGIN_PACKAGE;
+const char *plugin_rtmp_get_package(void) {
+  return PLUGIN_RTMP_PACKAGE;
 }
 
-static plugin_session *plugin_lookup_session(janus_plugin_session *handle) {
-  plugin_session *session = NULL;
+static plugin_rtmp_session *plugin_rtmp_lookup_session(janus_plugin_session *handle) {
+  plugin_rtmp_session *session = NULL;
   if (g_hash_table_contains(sessions, handle)) {
-    session = (plugin_session *)handle->plugin_handle;
+    session = (plugin_rtmp_session *)handle->plugin_handle;
   }
   return session;
 }
 
-void plugin_create_session(janus_plugin_session *handle, int *error) {
+void plugin_rtmp_create_session(janus_plugin_session *handle, int *error) {
   if (g_atomic_int_get(&stopping) || !g_atomic_int_get(&initialized)) {
     *error = -1;
     return;
   }
 
-  plugin_session *session = g_malloc0(sizeof(plugin_session));
+  plugin_rtmp_session *session = g_malloc0(sizeof(plugin_rtmp_session));
   session->handle = handle;
   session->started = FALSE;
   session->pipeline = NULL;
@@ -203,7 +202,7 @@ void plugin_create_session(janus_plugin_session *handle, int *error) {
   return;
 }
 
-void plugin_destroy_session(janus_plugin_session *handle, int *error) {
+void plugin_rtmp_destroy_session(janus_plugin_session *handle, int *error) {
   if (g_atomic_int_get(&stopping) || !g_atomic_int_get(&initialized)) {
     *error = -1;
     return;
@@ -211,7 +210,7 @@ void plugin_destroy_session(janus_plugin_session *handle, int *error) {
 
   janus_mutex_lock(&sessions_mutex);
 
-  plugin_session *session = plugin_lookup_session(handle);
+  plugin_rtmp_session *session = plugin_rtmp_lookup_session(handle);
   if (!session) {
     JANUS_LOG(LOG_ERR, "No Live session associated with this handle...\n");
     *error = -2;
@@ -225,7 +224,7 @@ void plugin_destroy_session(janus_plugin_session *handle, int *error) {
   return;
 }
 
-json_t *plugin_query_session(janus_plugin_session *handle) {
+json_t *plugin_rtmp_query_session(janus_plugin_session *handle) {
   json_t *result;
 
   if (!g_atomic_int_get(&stopping) && g_atomic_int_get(&initialized) && session_from_handle(handle) != NULL) {
@@ -238,14 +237,14 @@ json_t *plugin_query_session(janus_plugin_session *handle) {
 
 
 
-struct janus_plugin_result *plugin_handle_message(janus_plugin_session *handle, char *transaction, json_t *message, json_t *jsep) {
+struct janus_plugin_result *plugin_rtmp_handle_message(janus_plugin_session *handle, char *transaction, json_t *message, json_t *jsep) {
   janus_plugin_result *result;
 
   // Check we aren't stopping and initialized
   if (g_atomic_int_get(&stopping) || !g_atomic_int_get(&initialized)) {
     result = janus_plugin_result_new(JANUS_PLUGIN_ERROR, g_atomic_int_get(&stopping) ? "Shutting down" : "Plugin not initialized", NULL);
   } else {
-    plugin_session *session = session_from_handle(handle);
+    plugin_rtmp_session *session = session_from_handle(handle);
 
     if (session) {
       result = handle_message(session, message);
@@ -263,14 +262,14 @@ struct janus_plugin_result *plugin_handle_message(janus_plugin_session *handle, 
   return result;
 }
 
-void plugin_setup_media(janus_plugin_session *handle) {
-  JANUS_LOG(LOG_INFO, "[%s-%p] WebRTC media is now available\n", PLUGIN_PACKAGE, handle);
+void plugin_rtmp_setup_media(janus_plugin_session *handle) {
+  JANUS_LOG(LOG_INFO, "[%s-%p] WebRTC media is now available\n", PLUGIN_RTMP_PACKAGE, handle);
 }
 
-void plugin_hangup_media(janus_plugin_session *handle) {
-  JANUS_LOG(LOG_INFO, "[%s-%p] No WebRTC media anymore\n", PLUGIN_PACKAGE, handle);
+void plugin_rtmp_hangup_media(janus_plugin_session *handle) {
+  JANUS_LOG(LOG_INFO, "[%s-%p] No WebRTC media anymore\n", PLUGIN_RTMP_PACKAGE, handle);
   janus_mutex_lock(&sessions_mutex);
-  plugin_session *session = plugin_lookup_session(handle);
+  plugin_rtmp_session *session = plugin_rtmp_lookup_session(handle);
   stop_session_pipeline(session);
   janus_mutex_unlock(&sessions_mutex);
 }
@@ -279,7 +278,7 @@ void plugin_hangup_media(janus_plugin_session *handle) {
 // Message handlers
 // ------------------------------------------------------------------------------------------------
 
-janus_plugin_result *handle_message(plugin_session *session, json_t *root) {
+static janus_plugin_result *handle_message(plugin_rtmp_session *session, json_t *root) {
   janus_plugin_result *result;
 
   /* Increase the reference counter for this session: we'll decrease it after we handle the message */
@@ -291,7 +290,7 @@ janus_plugin_result *handle_message(plugin_session *session, json_t *root) {
   char error_cause[512];
 
   /* Get the request first */
-  JANUS_VALIDATE_JSON_OBJECT(root, request_parameters,
+  JANUS_VALIDATE_JSON_OBJECT(root, plugin_rtmp_request_parameters,
     error_code, error_cause, TRUE,
     ERROR_MISSING_ELEMENT, ERROR_INVALID_ELEMENT);
   // TODO: do something with error_code / error_cause from parser
@@ -313,8 +312,8 @@ janus_plugin_result *handle_message(plugin_session *session, json_t *root) {
   return result;
 }
 
-janus_plugin_result *handle_message_start(plugin_session *session, json_t* root) {
-  JANUS_LOG(LOG_INFO, "[%s] Handling start\n", PLUGIN_PACKAGE);
+static janus_plugin_result *handle_message_start(plugin_rtmp_session *session, json_t* root) {
+  JANUS_LOG(LOG_INFO, "[%s] Handling start\n", PLUGIN_RTMP_PACKAGE);
 
   json_t *value = json_object_get(root, "url");
   const char *url = json_string_value(value);
@@ -350,8 +349,8 @@ janus_plugin_result *handle_message_start(plugin_session *session, json_t* root)
 }
 
 
-janus_plugin_result *handle_message_stop(plugin_session *session, json_t *root) {
-  JANUS_LOG(LOG_INFO, "[%s] Handling stop\n", PLUGIN_PACKAGE);
+static janus_plugin_result *handle_message_stop(plugin_rtmp_session *session, json_t *root) {
+  JANUS_LOG(LOG_INFO, "[%s] Handling stop\n", PLUGIN_RTMP_PACKAGE);
 
   if (session->pipeline == NULL) {
     return janus_plugin_result_new(JANUS_PLUGIN_ERROR, "Live streaming hasn't been started", NULL);
@@ -366,7 +365,7 @@ janus_plugin_result *handle_message_stop(plugin_session *session, json_t *root) 
 }
 
 
-void stop_session_pipeline(plugin_session *session) {
+static void stop_session_pipeline(plugin_rtmp_session *session) {
   janus_mutex_lock(&session->mutex);
 
   if (session->pipeline) {
@@ -389,31 +388,60 @@ void stop_session_pipeline(plugin_session *session) {
 // Utility
 // ------------------------------------------------------------------------------------------------
 
-json_t *create_error_response(int error_code, char *error_cause) {
-  char msg[512];
-  g_snprintf(msg, 512, "%s", error_cause);
-
-  json_t *event = json_object();
-  json_object_set_new(event, "live", json_string("event"));
-  json_object_set_new(event, "error_code", json_integer(error_code));
-  json_object_set_new(event, "error", json_string(msg));
-
-  return event;
-}
-
-plugin_session *session_from_handle(janus_plugin_session *handle) {
-  plugin_session *session;
+static plugin_rtmp_session *session_from_handle(janus_plugin_session *handle) {
+  plugin_rtmp_session *session;
 
   janus_mutex_lock(&sessions_mutex);
-  session = plugin_lookup_session(handle);
+  session = plugin_rtmp_lookup_session(handle);
   janus_mutex_unlock(&sessions_mutex);
 
   return session;
 }
 
+// Validates it's RTMP(S) URL.
+static gboolean is_valid_url(const char *url) {
+  return g_regex_match_simple("^rtmps?://.+", url, 0, 0);
+}
+
 // ------------------------------------------------------------------------------------------------
-// Pipeline bus callback function
+// Pipeline
 // ------------------------------------------------------------------------------------------------
+
+static GstElement *create_pipeline(const char *url, int audio_port, int video_port) {
+  GstElement *pipeline;
+
+  gchar *pipeline_def = g_strdup_printf(
+    "rtpbin name=rtpbin "
+    "udpsrc address=localhost port=%d caps=\"application/x-rtp, media=audio, encoding-name=OPUS, clock-rate=48000\" ! rtpbin.recv_rtp_sink_1 "
+    "udpsrc address=localhost port=%d caps=\"application/x-rtp, media=video, encoding-name=H264, clock-rate=90000\" ! rtpbin.recv_rtp_sink_0 "
+    "rtpbin. ! rtph264depay ! flvmux streamable=true name=mux ! rtmpsink location=\"%s\" "
+    "rtpbin. ! rtpopusdepay ! queue ! opusdec ! voaacenc bitrate=128000 ! mux.",
+    audio_port, video_port, url);
+  JANUS_LOG(LOG_INFO, "Pipeline definition: %s\n", pipeline_def);
+
+  pipeline = gst_parse_launch(pipeline_def, NULL);
+
+  g_free(pipeline_def);
+
+  return pipeline;
+}
+
+
+static GstElement* start_pipeline(const char* url, int audio_port, int video_port) {
+  GstElement *pipeline = create_pipeline(url, audio_port, video_port);
+
+  if (!pipeline) {
+    JANUS_LOG(LOG_ERR, "Could not create the pipeline");
+    return NULL;
+  }
+
+  JANUS_LOG(LOG_INFO, "Pipeline started (ports audio: %d video: %d)\n", audio_port, video_port);
+
+  // Start playing
+  gst_element_set_state(pipeline, GST_STATE_PLAYING);
+
+  return pipeline;
+}
 
 static gboolean bus_callback(GstBus *bus, GstMessage *message, gpointer data) {
   GError *err;
@@ -448,10 +476,5 @@ static gboolean bus_callback(GstBus *bus, GstMessage *message, gpointer data) {
       break;
   }
   return TRUE;
-}
-
-// Validates it's RTMP(S) URL.
-gboolean is_valid_url(const char *url) {
-  return g_regex_match_simple("^rtmps?://.+", url, 0, 0);
 }
 
